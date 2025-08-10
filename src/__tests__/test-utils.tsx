23892cf09dc8a0246ab769dcdb0ce89899c69e09
dkhorev <me@dkhorev.pro>
2025-08-10 16:39:57 +0300

    chore: remove react-query logic

--- src/__tests__/test-utils.tsx
+++ src/__tests__/test-utils.tsx
@@ -1,48 +1,27 @@
 import { render, RenderOptions } from '@testing-library/react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactElement } from 'react';
-import { ToastProvider } from './components/ui/toast';
-import { ToastContext } from './contexts/toast';
+import { ToastProvider } from '../components/ui/toast';
+import { ToastContext } from '../contexts/toast';
 
-const createTestQueryClient = () =>
-  new QueryClient({
-    defaultOptions: {
-      queries: {
-        retry: false,
-        staleTime: 0,
-        gcTime: 0,
-      },
-      mutations: {
-        retry: false,
-      },
-    },
-  });
-
-interface CustomRenderOptions extends Omit<RenderOptions, 'wrapper'> {
-  queryClient?: QueryClient;
-}
+type CustomRenderOptions = Omit<RenderOptions, 'wrapper'>;
 
 export function renderWithProviders(
   ui: ReactElement,
   options: CustomRenderOptions = {},
 ) {
-  const { queryClient = createTestQueryClient(), ...renderOptions } = options;
-
   const mockToastContext = {
     showToast: jest.fn(),
   };
 
   function Wrapper({ children }: { children: React.ReactNode }) {
     return (
-      <QueryClientProvider client={queryClient}>
-        <ToastContext.Provider value={mockToastContext}>
-          <ToastProvider>{children}</ToastProvider>
-        </ToastContext.Provider>
-      </QueryClientProvider>
+      <ToastContext.Provider value={mockToastContext}>
+        <ToastProvider>{children}</ToastProvider>
+      </ToastContext.Provider>
     );
   }
 
-  return render(ui, { wrapper: Wrapper, ...renderOptions });
+  return render(ui, { wrapper: Wrapper, ...options });
 }
 
 export * from '@testing-library/react';
